d17f082e3b99daa1f0a794796953253d23188b96
Numan Ali <alinuman4347@gmail.com>
2026-08-07 01:04:05 +0500

    Bump header logo further (66px -> 92px) per follow-up feedback that it was still too small

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar() {
       <nav className={`navbar${scrolled ? ' navbar--scrolled' : ''}`}>
         <div className="pad navbar__inner">
           <a href="#home" className="navbar__logo">
-            <img src={logoWhite} alt="Gen Z Mobile Hub" height={66} />
+            <img src={logoWhite} alt="Gen Z Mobile Hub" height={92} />
           </a>
           <div className="navbar__links">
             {NAV_LINKS.map((link) => (
